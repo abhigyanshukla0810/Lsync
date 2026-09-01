a03896f9fc86135b80fed3fa865a526970f7d2b2
Abhigyan Shukla <ayush1abhigyan@gmail.com>
2026-09-02 02:43:16 +0530

    Time: 2 ms (72.65%) | Memory: 14.8 MB (43.78%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -4,14 +4,11 @@ public:
         unordered_map <int,int> mpp;
         int x = 0,i = 0;
         int n = nums.size();
-        for(i = 0; i<n;i++)mpp[nums[i]] = i;
         for(i = 0; i<n;i++){
             x = target - nums[i];
-            if(mpp.find(x) != mpp.end() && mpp[x] != i) break;
+            if(mpp.find(x) != mpp.end()) return {mpp[x], i};
+            mpp[nums[i]] = i;
         }
-        int a = mpp[x]; 
-        int b = i;
-        return {a,b};
-                
+        return {};
     };
 };
