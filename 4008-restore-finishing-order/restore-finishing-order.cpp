58ee80abdb5d65c580c49bb1a016b606245f2f9d
Abhigyan Shukla <ayush1abhigyan@gmail.com>
2026-09-22 13:46:07 +0530

    Time: 0 ms (100.00%) | Memory: 50.6 MB (24.25%) - LeetSync

--- 4008-restore-finishing-order/restore-finishing-order.cpp
+++ 4008-restore-finishing-order/restore-finishing-order.cpp
@@ -3,10 +3,10 @@ public:
     vector<int> recoverOrder(vector<int>& s, vector<int>& friends) {
         unordered_set<int> stt;
         for(int x : friends)stt.insert(x);
-        for(int i = 0; i<s.size();){
-            if(stt.find(s[i]) == stt.end()) s.erase(s.begin()+i);
-            else i++;
+        vector <int> v;
+        for(int x : s){
+            if(stt.find(x) != stt.end()) v.emplace_back(x);
         }
-        return s;
+        return v;
     }
 };
